230db9b0bc5382af34913b5b265fc0c3e31d01b2
tomchuk55 <65248076+tomchuk55@users.noreply.github.com>
2020-05-27 20:14:23 +0300

    Add files via upload

--- TorNode.py
+++ TorNode.py
@@ -6,6 +6,8 @@ PORT = config.PORT
 
 def portFinder(ip):
     global PORT
+    print(ip)
+    print(PORT)
     n = socket.socket()
     PORT += 1
     if PORT == 10000:
@@ -43,27 +45,34 @@ def tDecode(data, key):
     return newdata
 
 
-def nodeHandler(c, data, key):
+def nodeHandler(s, data, key):
     global PORT
     message = tDecode(data, key)
+    print(message)
     i = message.find(';')
     if i == -1:
         ip = message
         print(ip)
         webbrowser.open(ip)
+        ip = "http://" + ip
         fp = urllib.request.urlopen(ip)
-        message = fp.read().decode("utf8")
+        message = fp.read().decode("utf8") + "done"
         fp.close()
     else:
         ip = message[:i]
         message = message[i+1:]
-        n = portFinder(ip)
+        #n = portFinder(ip)
+        n = socket.socket()
+        n.connect((ip, PORT))
         print(message)
         n.send(message.encode())
-        message = n.recv(9999999).decode()
-        message = tEncode(message, key)
+        message = n.recv(1024).decode()
+        while(message[-4:] != "done"):
+            message += n.recv(1024).decode()
+        message = message[:-4]
+        message = tEncode(message, key) + "done"
     print(message)
-    c.send(message.encode())
+    s.send(message.encode())
 
 
 
@@ -78,12 +87,13 @@ def main():
     server.listen(5)
     while True:
         try:
-            c, addr = server.accept()
-            s.recv(0).decode()
+            s.recv(1).decode()
             data = s.recv(1024).decode()
             print(data)
             if data == "confirmed":
+                c, addr = server.accept()
                 data = c.recv(9999999).decode()
+                print(data)
                 threading.Thread(target=nodeHandler, args=(c, data, key)).start()
         except:
             continue
